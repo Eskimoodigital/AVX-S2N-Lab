673278b5776b7eaf485cfec3ee999d1340637df6
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2022-06-03 16:26:05 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "fw_management_ips_azure" {
   value = nonsensitive(module.transit_adoption_framework.firenet["azure_transit_firenet"].aviatrix_firewall_instance.*.public_ip)
 }
 
-output "fw_management_ips_gcp" {
-  value = nonsensitive(module.transit_adoption_framework.firenet["gcp_transit_firenet"].aviatrix_firewall_instance.*.public_ip)
+# output "fw_management_ips_gcp" {
+#   value = nonsensitive(module.transit_adoption_framework.firenet["gcp_transit_firenet"].aviatrix_firewall_instance.*.public_ip)
 }
 
