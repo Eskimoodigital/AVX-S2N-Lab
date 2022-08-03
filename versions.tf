e61f84b1b1843f064911c30d6d6278aa5ba3ed46
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2022-08-03 09:26:15 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "2.22.0"
+      version = "2.22.1"
     }
   }
   required_version = ">=0.15"
